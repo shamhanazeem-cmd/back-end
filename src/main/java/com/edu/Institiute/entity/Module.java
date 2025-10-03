28ab8721a6d01793313949a2f5b6b3e421d7fd61
shamhanazeem-cmd <shamhanazeem@gmail.com>
2025-10-04 01:58:19 +0530

    Project Environment Added.

--- src/main/java/com/edu/Institiute/entity/Module.java
+++ src/main/java/com/edu/Institiute/entity/Module.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
